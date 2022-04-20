996dc14919db9e00659a756927be8b0e2d8267bb
fposca <poscafer@gmail.com>
2022-04-19 22:13:05 -0300

    first image

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta es la app de Fer Posca</Text>
+      <Text style={styles.baseText}>Esta es la app de Fer Posca</Text>
+      <Image
+          style={{ width: 400, height: 400, marginBottom: 15 }}
+          source={require("./assets/rick.png")}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  baseText: {
+    color: '#fff',
+    fontSize: '2em',
+    textAlign: 'center'
+
+  }
 });
